612edbda599f9d687358c2fdff421d7b05014bee
HiryuJupiter <xooase@gmail.com>
2021-04-16 20:45:28 +1000

    car looking

--- Assets/Scripts/ObjectMove.cs
+++ Assets/Scripts/ObjectMove.cs
@@ -5,40 +5,42 @@ using UnityEngine;
 public class ObjectMove : MonoBehaviour
 {
     public Transform[] target;
-    public float speed;
+    public float moveSpeed = 2f;
+    public float rotationSpeed = 5f;
 
     private int current;
 
-    Quaternion lookAtSlowly(Transform t, Vector3 target, float turnSpeed)
+    Quaternion lookAtSlowly(Vector3 targetPos)
     {
-
         //(t) is the gameobject transform
         //(target) is the location that (t) is going to look at
         //speed is the quickness of the rotation
 
-        Vector3 relativePos = target - t.position;
-        Quaternion toRotation = Quaternion.LookRotation(relativePos);
-        return Quaternion.Lerp(t.rotation, toRotation, turnSpeed * Time.deltaTime);
+        Vector3 relativePos = targetPos - transform.position;
+        Quaternion toRotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        return Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
     }
-
-    //void FixedUpdate()
-    //{
-    //    transform.rotation = lookAtSlowly(transform, target[current].position, 1);
-    //}
 
     private void LateUpdate()
     {
-        if(transform.position != target[current].position)
+        if(Vector3.Distance(transform.position, targetPos) > 0.1f)
         {
-            Vector3 position = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
-            GetComponent<Rigidbody>().MovePosition(position);
-            //transform.LookAt(target[current].position);
-            transform.rotation = lookAtSlowly(transform, target[current].position, 2);
-            transform.position = position;
+            Vector3 pos = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+
+            transform.rotation = lookAtSlowly(targetPos);
+            transform.position = pos;
         }
         else
         {
             current = (current + 1) % target.Length;
         }
     }
+
+    Vector3 targetPos => target[current].position;
+
+    //private void OnGUI()
+    //{
+    //    GUI.Label(new Rect(20, 20, 200, 20), current.ToString());
+    //}
 }
